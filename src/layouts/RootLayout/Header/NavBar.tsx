1add19667bc86d6a95962da3145dbcd0166ae7b8
이호준 <ljun@ihojun-ui-Macmini.local>
2023-10-05 14:48:20 +0900

    (fix): about 404 수정

--- src/layouts/RootLayout/Header/NavBar.tsx
+++ src/layouts/RootLayout/Header/NavBar.tsx
@@ -2,7 +2,7 @@ import styled from "@emotion/styled"
 import Link from "next/link"
 
 const NavBar: React.FC = () => {
-  const links = [{ id: 1, name: "About", to: "/about" }]
+  const links = [{ id: 1, name: "Introduction", to: "/introduction" }]
   return (
     <StyledWrapper className="">
       <ul>
